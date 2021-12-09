a4e77b73c926acb2d244dd070a113078f8e891ed
PieckaAndrej <93518895+PieckaAndrej@users.noreply.github.com>
2021-12-09 12:32:06 +0100

    Tests

---
+++ src/controller/OrderCtrlTest.java
@@ -0,0 +1,34 @@
+package controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OrderCtrlTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testCreateOrder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreateOrderline() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFinishOrder() {
+		fail("Not yet implemented");
+	}
+
+}
