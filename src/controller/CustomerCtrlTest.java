1cacdc6e3e40dcc8900dcb195b7cd1d2b73ddd95
Goorson <10407543@ucn.dk>
2021-12-09 12:37:23 +0100

    Create CustomerCtrlTest.java

---
+++ src/controller/CustomerCtrlTest.java
@@ -0,0 +1,14 @@
+package controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CustomerCtrlTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
