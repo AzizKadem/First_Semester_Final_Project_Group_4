29313f45899515a016139ea7d1aed53ec0dfa90d
Goorson <10407543@ucn.dk>
2022-01-21 12:46:20 +0100

    Update MainGUI.java

--- src/ui/MainGUI.java
+++ src/ui/MainGUI.java
@@ -9,6 +9,10 @@ import javax.swing.border.EmptyBorder;
 import java.awt.GridLayout;
 import javax.swing.BoxLayout;
 import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import javax.swing.Box;
+import javax.swing.JButton;
+import java.awt.Font;
 
 public class MainGUI extends JFrame {
 
@@ -39,11 +43,36 @@ public class MainGUI extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setLayout(null);
 		
 		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.NORTH);
-		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		panel.setBounds(10, 10, 426, 32);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Main Menu");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setBounds(10, 10, 80, 13);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Time");
+		lblNewLabel_1.setBounds(357, 10, 45, 13);
+		panel.add(lblNewLabel_1);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(10, 52, 104, 201);
+		contentPane.add(panel_1);
+		
+		JButton btnNewButton = new JButton("Orders");
+		panel_1.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Leases");
+		panel_1.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Ya mama");
+		panel_1.add(btnNewButton_2);
+		
+		Box verticalBox = Box.createVerticalBox();
+		panel_1.add(verticalBox);
 	}
-
 }
