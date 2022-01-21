ea4a1073ec762d86d370a0cfd5a0a7a6663e6e41
Goorson <10407543@ucn.dk>
2022-01-21 13:32:38 +0100

    Update MainGUI.java
    essa

--- src/ui/MainGUI.java
+++ src/ui/MainGUI.java
@@ -13,16 +13,23 @@ import javax.swing.JLabel;
 import javax.swing.Box;
 import javax.swing.JButton;
 import java.awt.Font;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
 public class MainGUI extends JFrame {
 
 	private JPanel contentPane;
-
+	private JPanel panel_2;
+	private JPanel panel_3;
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
+			
 			public void run() {
 				try {
 					MainGUI frame = new MainGUI();
@@ -32,6 +39,7 @@ public class MainGUI extends JFrame {
 				}
 			}
 		});
+		
 	}
 
 	/**
@@ -43,36 +51,87 @@ public class MainGUI extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 10, 426, 32);
-		contentPane.add(panel);
-		panel.setLayout(null);
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		contentPane.add(panel, BorderLayout.CENTER);
 		
-		JLabel lblNewLabel = new JLabel("Main Menu");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel.setBounds(10, 10, 80, 13);
-		panel.add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("Time");
-		lblNewLabel_1.setBounds(357, 10, 45, 13);
-		panel.add(lblNewLabel_1);
+		
+		panel_2 = new JPanel();
+		panel_2.setVisible(false);
+		panel.add(panel_2);
+		
+		Box verticalBox_1 = Box.createVerticalBox();
+		panel_2.add(verticalBox_1);
+		
+		JLabel lblNewLabel_1 = new JLabel("Orders");
+		verticalBox_1.add(lblNewLabel_1);
+		
+		JButton btnNewButton_2 = new JButton("Create order");
+		verticalBox_1.add(btnNewButton_2);
+		
+		panel_3 = new JPanel();
+		panel_3.setVisible(false);
+		panel.add(panel_3);
+		
+		Box verticalBox_2 = Box.createVerticalBox();
+		panel_3.add(verticalBox_2);
+		
+		JLabel lblNewLabel_2 = new JLabel("Leases");
+		verticalBox_2.add(lblNewLabel_2);
+		
+		JButton btnNewButton_3 = new JButton("Create Lease");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		verticalBox_2.add(btnNewButton_3);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(10, 52, 104, 201);
-		contentPane.add(panel_1);
-		
-		JButton btnNewButton = new JButton("Orders");
-		panel_1.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Leases");
-		panel_1.add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("Ya mama");
-		panel_1.add(btnNewButton_2);
+		contentPane.add(panel_1, BorderLayout.WEST);
 		
 		Box verticalBox = Box.createVerticalBox();
 		panel_1.add(verticalBox);
+		
+		JButton btnNewButton = new JButton("Orders");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				openOrders();
+			}
+		});
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		verticalBox.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Leases");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				openLeases();
+			}
+		});
+		btnNewButton_1.setPreferredSize(new Dimension(50, 100));
+		btnNewButton_1.setIcon(null);
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		verticalBox.add(btnNewButton_1);
+		
+		JLabel lblNewLabel = new JLabel("Main menu");
+		contentPane.add(lblNewLabel, BorderLayout.NORTH);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
+	}
+	
+	private void openOrders() {
+		hidePanels();
+		panel_2.setVisible(true);
+	}
+	
+	private void openLeases() {
+		hidePanels();
+		panel_3.setVisible(true);
+	}
+	
+	private void hidePanels() {
+		panel_2.setVisible(false);
+		panel_3.setVisible(false);
 	}
 }
